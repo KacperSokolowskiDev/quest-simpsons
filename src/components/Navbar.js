d559838be242682f6e2b1dc140e957aae1d3cd2e
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-10-13 11:22:27 +0200

    Created a Navbar JS and CSS file

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar () {
+    return (
+        <nav className="Navbar"><a href="https://en.wikipedia.org/wiki/The_Simpsons">Simpsons Quotes</a></nav>
+    );
+}
+
+export default Navbar;
