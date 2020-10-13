52d524c1beb7bc84789beb3c6fddef9f7fe358ba
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-10-13 11:38:11 +0200

    Created a QuoteCard JS and CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import QuoteCard from './components/QuoteCard'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <QuoteCard />
     </div>
   );
 }
