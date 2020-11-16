bfc0c856cf70c9ad07a6f171348411c9f0942400
Kacper Sokolowski <Kacperbxl@gmail.com>
2020-11-16 10:32:33 +0100

    Finished React Quest 8, component didMount

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import axios from 'axios';
-import React, {Component} from 'react';
-import Navbar from './components/Navbar';
+import axios from "axios";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 // import QuoteList from './components/QuoteList';
 // import QuoteForm from './components/QuoteForm';
-import QuoteCard from './components/QuoteCard';
-
-const simpleQuote = {
-  quote: 'Oh Yeah!',
-  character: 'Duffman',
-  image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FDuffman.png?1497567511709',
-  
-};
+import QuoteCard from "./components/QuoteCard";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      simpsonQuote: simpleQuote
+      simpsonQuote: null,
     };
     this.getQuote = this.getQuote.bind(this);
-  };
-  getQuote = () => {
-    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
-      .then(response => response.data)
-      .then(data => {
-        this.setState({simpsonQuote: data[0]})
-        });
   }
-  render () {
-    return(
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getQuote = () => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ simpsonQuote: data[0] });
+      });
+  };
+  render() {
+    return (
       <div>
         <Navbar />
-        <QuoteCard simpsonQuote={this.state.simpsonQuote}/>
-        <button type="button" onClick={this.getQuote}>Next Quote</button>
+        {this.state.simpsonQuote ? (
+          <QuoteCard simpsonQuote={this.state.simpsonQuote} />
+        ) : (
+          <p>No Data Yet</p>
+        )}
+        <button type="button" onClick={this.getQuote}>
+          Next Quote
+        </button>
       </div>
     );
   }
 }
-
 
 // function App() {
 //   return (
